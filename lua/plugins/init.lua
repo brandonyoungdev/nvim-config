8f902345ec17da4cd96d4e31bc4eb37cfa3b8402
Brandon Young <32550081+Brandutchmen@users.noreply.github.com>
2025-06-12 18:13:08 -0700

    fix: keys

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -475,6 +475,7 @@ return {
   },
   {
     "mistweaverco/kulala.nvim",
+    keys = {
       { "<leader>Rs", desc = "Send request" },
       { "<leader>Ra", desc = "Send all requests" },
       { "<leader>Rb", desc = "Open scratchpad" },
